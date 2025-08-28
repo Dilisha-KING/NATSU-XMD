6f2261a3a31dfdb54d0f3dbdba3d4db4cd7aa99b
SCAR-LK <dgimshan79@gmail.com>
2025-08-28 10:41:27 +0530

    ★ Ｄ𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝙳 𝙱𝚈 𝙳𝙸𝙻𝙸𝚂𝙷𝙰 𝚃𝙴𝙲𝙷 ㋡

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Put Your Session Id",
+SESSION_ID: process.env.SESSION_ID || "𝙽𝙰𝚃𝚂𝚄=NBYCFb6C#eY8oTHwnuwLx0Q2chfWDpxQ_9dLi6VtrZ_hT13mrDH0",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
